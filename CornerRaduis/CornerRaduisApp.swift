5e6ff4cbf8f227a21b25adbbf6b6edf5c3cf0738
Omar Elsayed <eng.omar.elsayed@hotmail.com>
2023-11-26 16:57:38 +0200

    Initial Commit

---
+++ CornerRaduis/CornerRaduisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornerRaduisApp.swift
+//  CornerRaduis
+//
+//  Created by Eng.Omar Elsayed on 26/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CornerRaduisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
