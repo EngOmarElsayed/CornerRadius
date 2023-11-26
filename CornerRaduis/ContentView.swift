86cffe03158c4199daa0447d93e5d8d536aae1f9
Omar Elsayed <eng.omar.elsayed@hotmail.com>
2023-11-26 16:59:15 +0200

    Final

--- CornerRaduis/ContentView.swift
+++ CornerRaduis/ContentView.swift
@@ -7,18 +7,47 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct customeCornerRaduiss: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button {} label: {
+            Text("CornerRadius")
+                .foregroundStyle(.black)
+                .padding()
+                .customeCornerRaduis(topLeading: 20, bottomLeading: 0.0, bottomTrailing: 20, topTrailing: 20, color: .red)
+                
+            
         }
-        .padding()
+
     }
 }
 
+extension View {
+    func customeCornerRaduis(topLeading: CGFloat?,bottomLeading: CGFloat?,bottomTrailing: CGFloat?,topTrailing: CGFloat?,color: Color?) -> some View {
+        modifier(CustomeCornerRaduis(topLeading: topLeading,bottomLeading: bottomLeading,bottomTrailing: bottomTrailing,topTrailing: topTrailing,color: color))
+    }
+}
+public struct CustomeCornerRaduis: ViewModifier {
+    var topLeading: CGFloat?
+    var bottomLeading: CGFloat?
+    var bottomTrailing: CGFloat?
+    var topTrailing: CGFloat?
+    var color: Color?
+    
+  public  func body(content: Content) -> some View {
+        content
+            .background(
+                UnevenRoundedRectangle(cornerRadii:.init(
+                    topLeading: topLeading ?? 0.0,
+                    bottomLeading: bottomLeading ?? 0.0,
+                    bottomTrailing: bottomTrailing ?? 0.0,
+                    topTrailing: topTrailing ?? 0.0
+                )).foregroundStyle(color ?? .clear)
+            )
+    }
+}
+
+
 #Preview {
-    ContentView()
+    customeCornerRaduiss()
 }
